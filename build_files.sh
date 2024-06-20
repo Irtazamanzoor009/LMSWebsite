0cc7638910ece77d65575e00ea5261a04c4a43e4
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-06-20 22:23:31 +0500

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
 echo "BUILD END"
