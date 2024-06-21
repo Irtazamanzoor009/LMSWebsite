4d413a449b721083c0eecc7dbb27766de1d3a382
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-06-22 00:24:03 +0500

    up

--- LMSWebsite/settings.py
+++ LMSWebsite/settings.py
@@ -81,12 +81,20 @@ WSGI_APPLICATION = 'LMSWebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'VERCEL' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/tmp/db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
